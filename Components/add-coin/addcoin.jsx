26a508701c41a2194ec77a9af79548f893ae988a
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-08-17 23:23:13 +0530

    form created

--- Components/add-coin/addcoin.jsx
+++ Components/add-coin/addcoin.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const addcoin = () => {
   return (
-    <div>addcoin</div>
+    <>
+        <div>
+            <h1>Add Coin</h1>
+            <form>
+                <label>
+                    Symbol:
+                    <input type="text" name="symbol" />
+                </label>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Price:
+                    <input type="text" name="price" />
+                </label>
+                <label>
+                    Quantity:
+                    <input type="text" name="quantity" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            
+        </div>
+    </>
   )
 }
 
